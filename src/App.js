4b4e4c73748dbe1c81a67fbae88fb395ea0edb0a
Mohammed El Abdi <momo.elabdi@bluewin.ch>
2022-06-02 14:07:44 +0200

    user references

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import Home from "./Pages/Home";
+import ErrorPage from "./Pages/ErrorPage";
 
 function App() {
   return (
-    <div clasName="App"> Hello world</div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/profile">Profile</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/"  element={ <Home />}  />
+        <Route path="/about"  element={ <About />}  />
+        <Route path="/profile/:username"  element={ <Profile />}  />
+        <Route path="*"  element={ <ErrorPage />}  />
+
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/Profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useNavigate, useParams } from 'react-router-dom';
+
+function Profile() {
+  let { username } = useParams();
+  let navigate = useNavigate();
+  return (
+    <div>
+      This is the Profile page of { username }
+       <button onClick={ () => {navigate('/')} }>Change to home page </button>
+    </div>
+   );
+}
+
+export default Profile;
